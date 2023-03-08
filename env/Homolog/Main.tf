098b4c1e54e934dbbc2fd9df33b25a88804e0ae0
thaysesf <92764190+thaysesf@users.noreply.github.com>
2023-03-07 21:24:01 -0300

    Update Main.tf

--- env/Homolog/Main.tf
+++ env/Homolog/Main.tf
@@ -1,9 +1,20 @@
-module "prod" {
-    source = "../../infra/EKS.tf"
-    
-    cluster_name = "homolog2"
-}
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
 
-output "IP_db" {
-  value = module.prod.IP
+  cluster_name                    = "homolog2"
+  cluster_version                 = "1.24"
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  eks_managed_node_groups = {
+    alura = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 3
+      vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
+      instance_types = ["t2.micro"]
+    }
+  }
 }
